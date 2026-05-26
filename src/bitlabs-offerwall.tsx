b2bc9cd7549eaf8b71feeedaafc9085cd20f929e
Omar <omar.raad@prodege.com>
2026-05-26 15:01:53 +0200

    fix: remove stale event listener on init to prevent duplicate callbacks

--- src/bitlabs-offerwall.tsx
+++ src/bitlabs-offerwall.tsx
@@ -1,15 +1,21 @@
-import { NativeEventEmitter } from 'react-native';
+import { NativeEventEmitter, type EmitterSubscription } from 'react-native';
 import { NativeBitLabs } from './native-bitlabs';
 
 const BitLabsEmitter = new NativeEventEmitter(NativeBitLabs);
 
+let subscription: EmitterSubscription | null = null;
+
 const init = (token: string, uid: string) => {
   NativeBitLabs.configure(token, uid);
 
-  BitLabsEmitter.addListener('onOfferwallClosed', ({ reward }) => {
-    onReward(reward as number);
-    onOfferwallClosed();
-  });
+  subscription?.remove();
+  subscription = BitLabsEmitter.addListener(
+    'onOfferwallClosed',
+    ({ reward }) => {
+      onReward(reward as number);
+      onOfferwallClosed();
+    },
+  );
 };
 
 const requestTrackingAuthorization = NativeBitLabs.requestTrackingAuthorization;
